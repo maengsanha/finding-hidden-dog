9e3e849180ae1b4bc08505b40c9ba6066da786c0
joshua-dev <msh01175@gmail.com>
2020-06-21 04:38:47 +0900

    Upload cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
-
+// initialize Firebase SDK.
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+let db = admin.firestore();
+
+
+// insert inserts data into database.
+// public DNS(IPv4): https://us-central1-finding-hidden-dog.cloudfunctions.net/insert
+exports.insert = functions.https.onRequest((request, response) => {
+  const user = request.body.user_name;
+  const timeSpent = parseFloat(request.body.time_spent);
+
+  // add new data into database.
+  db.collection('scores')
+    .doc()
+    .set({
+      user_name: user,
+      time_spent: timeSpent
+    });
+})  // end insert.
+
+// get gets top ten score datas from database.
+// public DNS(IPv4): https://us-central1-finding-hidden-dog.cloudfunctions.net/get
+exports.get = functions.https.onRequest((request, response) => {
+  response.send(
+    db.collection('scores')
+      .where('time_spent', '>', 0)
+      .orderBy('time_spent')
+      .limit(10));
+})  // end get.
